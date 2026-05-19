90e93e578171443aa758f0e0effda5536c582f70
Sudhanshu1582001 <91254384+Sudhanshu1582001@users.noreply.github.com>
2026-05-19 17:00:17 +0530

    Create CareNote.jsx

---
+++ src/src/CareNote.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CareNote from './CareNote.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CareNote />
+  </React.StrictMode>
+)
